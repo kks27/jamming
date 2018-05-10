dfa1fb1ad09ccbd4cdb43f51159d701ed67c8cf5
Kiran Sandhu <sandhukiran15@gmail.com>
2018-05-09 19:43:12 -0700

    removed extra {

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends Component {
 
-    render(){
+    render() {
         let tracks = this.props.tracks ? this.props.tracks : [];
 
         return (
             <div className="TrackList">
                 {
                     tracks.map(track => {
-                        return <Track key={track.id}
-                                      track={track}
-                                      onAdd={this.props.onAdd}
-                                      onRemove={this.props.onRemove}
-                                        isRemoval={this.props.isRemoval} /> }
+                            return <Track key={track.id}
+                                          track={track}
+                                          onAdd={this.props.onAdd}
+                                          onRemove={this.props.onRemove}
+                                          isRemoval={this.props.isRemoval}/>
+                        }
                     )}
-                }
-
             </div>
         );
     }
